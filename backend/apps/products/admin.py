baa1317a13cc0a042e44115c5911b942c305b187
mdouglas <workmadmax@gmail.com>
2026-04-09 11:39:52 -0300

    add class productimage

--- backend/apps/products/admin.py
+++ backend/apps/products/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
+from .models import Product, ProductImage
+from apps.categories.models import Category
 
 # Register your models here.
 
-from .models import Product
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'stock', 'is_active', 'created_at')
-    list_filter = ('is_active', 'created_at')
+    list_filter = ('is_active', 'category', 'created_at')
     search_fields = ('name', 'description')
     prepopulated_fields = {'slug': ('name',)}
