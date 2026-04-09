1de27997a09cb4b0f270b6cd919406dbd3be73c5
mdouglas <workmadmax@gmail.com>
2026-04-09 11:04:03 -0300

    add field in product class

--- backend/apps/products/models.py
+++ backend/apps/products/models.py
@@ -4,8 +4,12 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(
         'categories.Category', on_delete=models.CASCADE, related_name='products'
     )
