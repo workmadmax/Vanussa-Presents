eb3013e3ae5252095f9f53d7d7713169be2db2ce
mdouglas <workmadmax@gmail.com>
2026-04-09 11:21:20 -0300

    add class meta for category

--- backend/apps/categories/models.py
+++ backend/apps/categories/models.py
@@ -6,5 +6,9 @@ class Category(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
 
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return (self.name)
