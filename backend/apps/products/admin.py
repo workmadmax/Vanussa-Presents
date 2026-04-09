f3aa07526308d0b45fde124270dc45e446e449c8
mdouglas <workmadmax@gmail.com>
2026-04-09 11:22:32 -0300

    add admin product

--- backend/apps/products/admin.py
+++ backend/apps/products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Product
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock', 'is_active', 'created_at')
+    list_filter = ('is_active', 'created_at')
+    search_fields = ('name', 'description')
+    prepopulated_fields = {'slug': ('name',)}
