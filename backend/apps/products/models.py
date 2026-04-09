0bd4bf704ba599fed1dd7eb59712cc5be9c06566
mdouglas <workmadmax@gmail.com>
2026-04-09 11:40:24 -0300

    modelagem da class

--- backend/apps/products/models.py
+++ backend/apps/products/models.py
@@ -14,4 +14,12 @@ class Product(models.Model):
         'categories.Category', on_delete=models.CASCADE, related_name='products'
     )
     def __str__(self):
-        return self.name
+        return (self.name)
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='product_images/')
+    alt_text = models.CharField(max_length=255, blank=True)
+    
+    def __str__(self):
+        return (f"Image for {self.product.name}")
