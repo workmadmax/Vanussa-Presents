67b045b5b1bc726f71e82c7ee3fa4aa61c0858c6
mdouglas <workmadmax@gmail.com>
2026-04-10 20:47:55 -0300

    setup tailwind css and global style (nao tava funcionando pq idk)

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -6,13 +6,15 @@
 /*   By: mdouglas <mdouglas@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 17:47:03 by mdouglas          #+#    #+#             */
-/*   Updated: 2026/04/10 17:30:59 by mdouglas         ###   ########.fr       */
+/*   Updated: 2026/04/10 20:39:35 by mdouglas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 import type { ReactNode } from "react";
 import type { Metadata } from "next";
 import { CartProvider } from "@/context/cartContext";
+import { Header } from "@/components/header";
+
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -21,11 +23,15 @@ export const metadata: Metadata = {
 };
 
 
-export default function RootLayout({ children }: any) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-br">
       <body>
-        <CartProvider>{children}</CartProvider>
+        <CartProvider>
+          <Header />
+          {children}
+          {/* <Footer /> */}
+        </CartProvider>
       </body>
     </html>
   );
