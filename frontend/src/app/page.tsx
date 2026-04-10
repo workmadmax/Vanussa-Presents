5baf5203d7b3a25e0ace08336ad7e9071ababf1a
mdouglas <workmadmax@gmail.com>
2026-04-10 10:19:13 -0300

    load img test backend ok

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,34 +6,34 @@
 /*   By: mdouglas <mdouglas@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 21:43:32 by mdouglas          #+#    #+#             */
-/*   Updated: 2026/04/09 22:18:46 by mdouglas         ###   ########.fr       */
+/*   Updated: 2026/04/10 09:58:50 by mdouglas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import { api } from '@/services/api';
+import { useEffect, useState } from "react";
+import { api } from "@/services/api";
+import { ProductCard } from "@/components/productCard";
 
 export default function Home() {
-  const [ products, setProducts ] = useState<any[]>([]);
-  
+  const [products, setProducts] = useState<any[]>([]);
+
   useEffect(() => {
-    api.get('products/').then((res) => {
+    api.get("products/").then((res) => {
       setProducts(res.data);
     });
   }, []);
 
-    return (
+  return (
     <main style={{ padding: 20 }}>
       <h1>Vanusa Presentes 💎</h1>
 
-      {products.map((product) => (
-        <div key={product.id} style={{ marginBottom: 20 }}>
-          <h2>{product.name}</h2>
-          <p>R$ {product.price}</p>
-        </div>
-      ))}
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </main>
   );
 }
