f06ce810260adb478da5f7b2733a7748bf6536f3
mdouglas <workmadmax@gmail.com>
2026-04-22 15:44:44 -0300

    add localhost and unoptimized: true

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '8000',
-        pathname: '/media/**',
-      },
-    ],
-  },
+	images: {
+		unoptimized: true,
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "127.0.0.1",
+				port: "8000",
+				pathname: "/media/**",
+			},
+			{
+				protocol: "http",
+				hostname: "localhost",
+				port: "8000",
+				pathname: "/media/**",
+			},
+		],
+	},
 };
 export default nextConfig;
