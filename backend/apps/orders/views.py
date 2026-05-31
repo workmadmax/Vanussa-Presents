e3207f2b63466164e3c063d0fe91e4f3eee2dda1
mdouglas <workmadmax@gmail.com>
2026-05-31 17:39:42 -0300

    feat(orders): add OrderCreateView, MyOrdersView and OrderDetailView

--- backend/apps/orders/views.py
+++ backend/apps/orders/views.py
@@ -6,26 +6,14 @@
 #    By: mdouglas <mdouglas@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/04/24 21:49:29 by mdouglas          #+#    #+#              #
-#    Updated: 2026/04/25 14:30:22 by mdouglas         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    views.py                                           :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: mdouglas <mdouglas@student.42sp.org.br>    +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/04/24 21:49:29 by mdouglas          #+#    #+#              #
-#    Updated: 2026/04/25 00:00:00 by mdouglas         ###   ########.fr        #
+#    Updated: 2026/05/31 17:14:26 by mdouglas         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import ValidationError, NotFound
 from rest_framework import status
 
 from .models import Order
@@ -38,10 +26,15 @@ class OrderCreateView(APIView):
 
     def post(self, request):
         items = request.data.get("items", [])
-        order = OrderService(request.user, items)
+        try:
+            order = OrderService(request.user, items).create_order()
+        except NotFound as e:
+            return Response({"detail": str(e)}, status=status.HTTP_404_NOT_FOUND)
+        except ValidationError as e:
+            return Response({"detail": e.detail}, status=status.HTTP_400_BAD_REQUEST)
+        
         serializer = OrderSerializer(order)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
 
 class MyOrdersView(APIView):
     permission_classes = [IsAuthenticated]
@@ -59,7 +52,11 @@ class OrderDetailView(APIView):
 
     def get(self, request, pk):
         try:
-            order = Order.objects.get(pk=pk, user=request.user)
+            order = (
+                Order.objects
+                .prefetch_related("items__product")
+                .get(pk=pk, user=request.user)
+            )
         except Order.DoesNotExist:
             return Response(
                 {"detail": "Order not found"}, status=status.HTTP_404_NOT_FOUND
